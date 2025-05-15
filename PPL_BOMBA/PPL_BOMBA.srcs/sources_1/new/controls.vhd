4c4ae6b77d086a9af9f3eb6bb4872b5406038963
Nick Freisinger <frn47103@ei-vm-018.othr.de>
2025-05-15 13:01:08 +0200

    Main Structure

---
+++ PPL_BOMBA/PPL_BOMBA.srcs/sources_1/new/controls.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/15/2025 12:56:47 PM
+-- Design Name: 
+-- Module Name: controls - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity controls is
+    Port ( left : in STD_LOGIC;
+           right : in STD_LOGIC;
+           up : in STD_LOGIC;
+           down : in STD_LOGIC;
+           sel : in STD_LOGIC);
+end controls;
+
+architecture Behavioral of controls is
+
+begin
+
+
+end Behavioral;
