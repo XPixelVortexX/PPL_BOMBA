fd1289339154354d9268df17c2d81a16a9640fe8
XPixelVortexX <nick.freisinger@st.oth-regensburg.de>
2025-06-05 12:27:47 +0200

    Update memory.vhd

--- PPL_BOMBA/PPL_BOMBA.srcs/sources_1/new/memory.vhd
+++ PPL_BOMBA/PPL_BOMBA.srcs/sources_1/new/memory.vhd
@@ -1,14 +1,12 @@
 entity memory is
     generic (
-        OBJECT_SIZE : natural := 16;
-        PIXEL_SIZE : natural := 24
+        --
     );
     port (
-        video_active       : in  std_logic;
-        pixel_x, pixel_y   : in  std_logic_vector(OBJECT_SIZE-1 downto 0);
-        object1x, object1y : in  std_logic_vector(OBJECT_SIZE-1 downto 0);
-        object2x, object2y : in  std_logic_vector(OBJECT_SIZE-1 downto 0);
-        backgrnd_rgb       : in  std_logic_vector(PIXEL_SIZE-1 downto 0);
-        rgb                : out std_logic_vector(PIXEL_SIZE-1 downto 0)
+       -- 
     );
 end memory;
+
+architecture Behavioral of memory is
+    
+end Behavioral;
