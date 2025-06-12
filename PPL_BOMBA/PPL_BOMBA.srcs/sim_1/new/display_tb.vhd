5aee9f1a93473ca28b13c716b3d3664218cdadc9
Nick Freisinger <frn47103@hs-regensburg.de>
2025-06-12 12:54:10 +0200

    Update Testbench Display

--- PPL_BOMBA/PPL_BOMBA.srcs/sim_1/new/display_tb.vhd
+++ PPL_BOMBA/PPL_BOMBA.srcs/sim_1/new/display_tb.vhd
@@ -32,12 +32,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity display_tb is
---  Port ( );
 end display_tb;
 
 architecture Behavioral of display_tb is
 
+    component display is
+        port (
+        clk     : in  std_logic; -- 25 MHz VGA-Takt
+        vga_r   : out std_logic_vector(3 downto 0);
+        vga_g   : out std_logic_vector(3 downto 0);
+        vga_b   : out std_logic_vector(3 downto 0);
+        hsync   : out std_logic;
+        vsync   : out std_logic
+        );
+    end component;
+    
+  -- input
+  signal clk   : std_logic := '0';
+
+  -- output
+  signal vga_r, vga_g, vga_b    : std_logic_vector(3 downto 0);
+  signal hsync, vsync           : std_logic;
+  
 begin
 
+    clk <= not clk after 20ns;
 
+dut : display
+    port map(
+        clk     =>  clk,
+        vga_r   =>  vga_r,
+        vga_g   =>  vga_g,
+        vga_b   =>  vga_b,
+        hsync   =>  hsync,
+        vsync   =>  vsync
+        );
 end Behavioral;
