558e83d3abe6c97d976b94cb06ef0ce669e51090
lov43331 <lobingervali7@gmail.com>
2025-07-01 10:46:13 +0200

    update game_over

--- PPL_BOMBA/PPL_BOMBA.srcs/sources_1/new/game_logic.vhd
+++ PPL_BOMBA/PPL_BOMBA.srcs/sources_1/new/game_logic.vhd
@@ -10,12 +10,14 @@ entity game_logic is
     bomb_map      : in  std_logic_vector(63 downto 0);
     read_data     : in  std_logic_vector(1 downto 0);  -- z.?B. 00=verdeckt, 01=offen, 10=markiert
     write_data    : out std_logic_vector(1 downto 0);
-    write_enable  : out std_logic
+    write_enable  : out std_logic;
+    game_over     : out std_logic
   );
 end game_logic;
 
 architecture behavioral of game_logic is
-  signal addr : integer range 0 to 63;
+  signal addr           : integer range 0 to 63;
+  signal game_over_sig  : std_logic := '0';
 begin
 
   -- Adresse berechnen
@@ -24,23 +26,28 @@ begin
   process(clk)
   begin
     if rising_edge(clk) then
-      if click = '1' then
-        if read_data = "00" then  -- Noch nicht aufgedeckt
-          if bomb_map(addr) = '1' then
-            -- Bombe getroffen
-            write_data <= "11";  -- Beispiel: 11 = Bombe
+      if game_over_sig = "0" then
+        if click = '1' then
+          if read_data = "00" then  -- Noch nicht aufgedeckt
+            if bomb_map(addr) = '1' then
+              -- Bombe getroffen
+              write_data <= "11";  -- Beispiel: 11 = Bombe
+              game_over_sig <= '1';
+            else
+              -- Feld ist sicher
+              write_data <= "01";  -- Beispiel: 01 = Offen
+            end if;
+            write_enable <= '1';
           else
-            -- Feld ist sicher
-            write_data <= "01";  -- Beispiel: 01 = Offen
+            write_enable <= '0';
           end if;
-          write_enable <= '1';
         else
           write_enable <= '0';
-        end if;
-      else
-        write_enable <= '0';
-      end if;
+         end if;
+       end if;
     end if;
   end process;
+
+  game_over <= game_over_sig;
 
 end behavioral;
